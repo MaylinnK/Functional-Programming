7bbbde5c9edd53d93b06957e1c8df8dac6a9da07
MaylinnK <maylinn.kwakman@hva.nl>
2021-10-26 14:27:16 +0200

    Index.js aangemaakt, package.json aangemaakt
    Index.js haalt nu oogkleuren op en zet ze om naar kleine letters.

---
+++ index.js
@@ -0,0 +1,20 @@
+const data = require("./dataset.json");
+const old_key = "Wat is je oogkleur?";
+const new_key = "oogkleur";
+const oogkleuren = [];  
+
+for (let i = 0; i < data.length; i++) {
+  if (old_key !== new_key) {
+    Object.defineProperty(
+      data[i],
+      new_key,
+      Object.getOwnPropertyDescriptor(data[i], old_key)
+    );
+    delete data[i][old_key];
+  }
+}
+for (let i = 0; i < data.length; i++) {
+  oogkleuren.push(data[i].oogkleur.toLowerCase());
+}
+
+console.log(oogkleuren);
