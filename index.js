972ac7652f8d01814fdb8a1d37701693aaf01131
MaylinnK <maylinn.kwakman@hva.nl>
2021-10-27 13:17:05 +0200

    Geen node
    Node verwijderd. Index.js aangepast i.v.m. functional programming.

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const data = require("./dataset.json");
-const old_key = "Wat is je oogkleur?";
-const new_key = "oogkleur";
-const oogkleuren = [];
+function loopEdit(old_key, new_key, eyecolours) {
+  old_key = "Wat is je oogkleur?";
+  new_key = "eyecolour";
+  eyecolours = [];
 
-for (let i = 0; i < data.length; i++) {
-  if (old_key !== new_key) {
-    Object.defineProperty(
-      data[i],
-      new_key,
-      Object.getOwnPropertyDescriptor(data[i], old_key)
-    );
-    delete data[i][old_key];
-  }
-}
-for (let i = 0; i < data.length; i++) {
-  oogkleuren.push(data[i].oogkleur.replace(/\s/g, "").toLowerCase());
-}
+  fetch("./dataset.json")
+    .then((response) => response.json())
+    .then (function (data)
+  {
+    for (let i = 0; i < data.length; i++) {
+      if (old_key !== new_key) {
+        Object.defineProperty(
+          data[i],
+          new_key,
+          Object.getOwnPropertyDescriptor(data[i], old_key)
+        );
+        delete data[i][old_key];
+      }
+    }
+    for (let i = 0; i < data.length; i++) {
+      eyecolours.push(data[i].eyecolour.replace(/\s/g, "").toLowerCase());
+    }
+  });
 
-console.log(oogkleuren);
+  // console.log(eyecolours);
+  document.getElementById('test').innerHTML = eyecolours + "hey";
+}
+loopEdit();
